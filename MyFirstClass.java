a35b012da59d897afb72c2fc11b9cfda74aa261a
chetangirisd01 <161694087+chetangirisd01@users.noreply.github.com>
2024-08-02 22:17:05 +0530

    Update MyFirstClass.java

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -1,4 +1,5 @@
 // My First Java Program learning 
+//Java Program to print the simple statement
 public class MyFirstClass{
 	public static void main(String args[]){
 		System.out.println("This is a simple Java program");
