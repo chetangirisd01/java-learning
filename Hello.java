3e0d81ded872b96db0447c0ae3d40f352be17ac5
chetangirisd01 <chetangirisd@gmail.com>
2024-08-29 21:07:59 +0530

    ki

---
+++ Hello.java
@@ -0,0 +1,17 @@
+// Java program to Print Hello World Without Semicolon
+// Using if statement
+
+// Main class
+class Hello
+ {
+
+	// Main driver method
+	public static void main(String args[])
+	{
+
+		// Using if statement to
+		// print hello world
+		if (System.out.printf("Hello World") == null) {
+		}
+	}
+}
